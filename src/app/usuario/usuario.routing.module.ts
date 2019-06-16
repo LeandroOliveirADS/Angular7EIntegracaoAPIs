af68a0f48a00df43d17179ed0b62d30ddd6d1b38
Leandro Oliveira <leandroluis.cavalcantioliveira@gmail.com>
2019-06-15 23:38:25 -0300

    Formulário simples aula 11

--- src/app/usuario/usuario.routing.module.ts
+++ src/app/usuario/usuario.routing.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import{Routes, RouterModule} from '@angular/router'
 import { UsuarioComponent } from './usuario.component';
+import { UsuarioCadastroComponent } from './cadastro/usuario-cadastro.component';
 
 const routes: Routes = [
   { path: '', 
     component:UsuarioComponent
-  }
+  },
+  { path: 'add', 
+  component:UsuarioCadastroComponent
+}
 ];
 
 @NgModule({
   declarations: [],
   imports: [
     RouterModule.forChild(routes)
-  ]
+  ],
+  exports: [RouterModule]
 })
 export class UsuarioRoutingModule { }
